90bc285deb3a1c87b5a521f28620305a0f1dbcc3
CodeJourneys <caporter01@gmail.com>
2022-01-27 09:50:47 -0700

    Prag-Prog - Added solution to 14.1

--- Pragmatic-Programmer/14/14.1.cs
+++ Pragmatic-Programmer/14/14.1.cs
@@ -1,1 +1,24 @@
-// Add method to singly LinkedList that prints all elements in the list
+/**
+ * Add method to singly linked list that prints all elements in the list.
+ * Node definition below
+ *
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+void PrintValues()
+{
+    ListNode node = self.firstNode;
+
+    while(node.next != null)
+    {
+        Console.WriteLine(node.val);
+        node = node.next;
+    }
+}
