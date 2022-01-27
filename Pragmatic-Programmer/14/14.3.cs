7c656ccd9ae6b300b1784a673932d78ce7c9b798
CodeJourneys <caporter01@gmail.com>
2022-01-27 09:50:56 -0700

    Prag-Prog - Added solution to 14.3

--- Pragmatic-Programmer/14/14.3.cs
+++ Pragmatic-Programmer/14/14.3.cs
@@ -1,1 +1,25 @@
-// Add a method to singly LinkedList that returns the last element from the list
+/**
+ * Add method to singly linked list that returns the last element from the list.
+ * Node definition below
+ *
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+ListNode GetLastNode()
+{
+    ListNode node = self.firstNode;
+
+    while(node?.next != null)
+    {
+        node = node.next;
+    }
+
+    return node;
+}
