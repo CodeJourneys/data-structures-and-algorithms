ffe71c88024b8029390c5c73df847303efaf2ab5
CodeJourneys <caporter01@gmail.com>
2022-01-27 09:50:52 -0700

    Prag-Prog - Added solution to 14.2

--- Pragmatic-Programmer/14/14.2.cs
+++ Pragmatic-Programmer/14/14.2.cs
@@ -1,1 +1,27 @@
-// Add a method to DoublyLinkedList that prints all elements in reverse order
+/**
+ * Add method to doubly linked list that prints all elements in reverse order.
+ *
+ * Node definition below
+ *
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode prev;
+ *     public ListNode(int val=0, ListNode next=null, ListNode prev=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *         this.prev = prev;
+ *     }
+ * }
+ */
+
+ void PrintValuesInReverse()
+ {
+     ListNode node = self.lastNode;
+     
+     while(node.prev != null)
+     {
+         Console.WriteLine(node.val);
+         node = node.prev;
+     }
+ }
