29335ff0fa4267837abe26f93bb315adfa4c4822
CodeJourneys <caporter01@gmail.com>
2022-01-31 19:41:15 -0700

    Prag-Prog - Added solution to 14.4

--- Pragmatic-Programmer/14/14.4.cs
+++ Pragmatic-Programmer/14/14.4.cs
@@ -1,1 +1,30 @@
-// Add a method to singly LinkedList that reverses the list
+/**
+ * Add method to a singly linked list that reverses the list.
+ * Node definition below
+ *
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+void ReverseList()
+{
+    ListNode head = self.firstNode;
+    ListNode tail = null;
+    ListNode next;
+    
+    while(head.next != null)
+    {
+        next = head.next;
+        head.next = tail;
+        tail = head;
+        head = next;
+    }
+
+    self.firstNode = tail;
+}
