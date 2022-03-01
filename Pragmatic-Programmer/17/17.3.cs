e9f5cf8380f5363f46df813e545bcc0d272c9213
CodeJourneys <caporter01@gmail.com>
2022-02-28 21:16:31 -0700

    Prag-Prog - Proposed solution 17.3

--- Pragmatic-Programmer/17/17.3.cs
+++ Pragmatic-Programmer/17/17.3.cs
@@ -1,4 +1,28 @@
 /**
 * Write a function that traverses each node of a trie and prints each key, including all terminating ("*") keys.
 *
+* public class TrieNode 
+* {
+*     public Dictionary<char, TrieNode> children;
+
+*     public TrieNode(Dictionary<char, TrieNode> children = null) 
+*     {
+*         this.children = children ?? new Dictionary<char, TrieNode>();
+*     }
+* }
+*
 */
+
+
+void TraverseTrieAndPrintKeys(TrieNode node)
+{
+    foreach(var key in node.children.Keys)
+    {
+        Console.WriteLine(key);
+        
+        if(!key.Equals('*'))
+        {
+            TraverseTrieAndPrintKeys(node.children[key]);
+        }
+    }
+}
